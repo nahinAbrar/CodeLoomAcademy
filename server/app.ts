60caa84878d821d712317d3ea1e46aa062a45355
Asm Nahin <asmnahin21@gmail.com>
2024-07-07 19:58:05 +0600

    Connection Part Done

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,33 @@
-import express from "express";
-
+require('dotenv').config();
+import express, {NextFunction,Request,Response} from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 export const app = express();
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors => cross origin resource sharing
+app.use(cors({
+    origin: process.env.ORIGIN
+}));
+
+// testing api
+app.get("/test",(req:Request,res:Response,next:NextFunction) => {
+    res.status(200).json({
+        succsess: true,
+        message: "API is working",
+    });
+});
+
+
+// unknown root
+app.all("*", (req:Request,res:Response,next:NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+})
