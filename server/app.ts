647ae4e0a38f196213fa09d3cb3b8622621cc451
Asm Nahin <asmnahin21@gmail.com>
2024-07-07 21:37:56 +0600

    ErrorHandlerDone

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,8 @@
-require('dotenv').config();
-import express, {NextFunction,Request,Response} from "express";
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { ErrorMiddleWare } from "./middleware/error";
 
 export const app = express();
 
@@ -12,22 +13,25 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 // cors => cross origin resource sharing
-app.use(cors({
-    origin: process.env.ORIGIN
-}));
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
 
 // testing api
-app.get("/test",(req:Request,res:Response,next:NextFunction) => {
-    res.status(200).json({
-        succsess: true,
-        message: "API is working",
-    });
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    succsess: true,
+    message: "API is working",
+  });
 });
 
-
 // unknown root
-app.all("*", (req:Request,res:Response,next:NextFunction) => {
-    const err = new Error(`Route ${req.originalUrl} not found`) as any;
-    err.statusCode = 404;
-    next(err);
-})
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use(ErrorMiddleWare);
