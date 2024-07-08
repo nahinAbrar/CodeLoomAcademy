002e4808668b4a52dd5f6144c259c0b7672e125a
Asm Nahin <asmnahin21@gmail.com>
2024-07-08 10:47:22 +0600

    Registration and activation backend done

--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-import { registrationUser } from '../controllers/userController';
+import { registrationUser,activateUser } from '../controllers/userController';
 const userRouter = express.Router();
 
 userRouter.post('/registration', registrationUser);
+
+userRouter.post('/activate-user', activateUser);
 
 export default userRouter;
