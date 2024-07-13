d7104f489ab5296dd8a5cad942743142039248ac
Asm Nahin <asmnahin21@gmail.com>
2024-07-13 17:00:27 +0600

    Hero working

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,6 +2,7 @@
 import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
 import Header from "./components/Header";
+import Hero from "./components/Route/Hero";
 
 interface Props { }
 
@@ -22,6 +23,8 @@ const Page: FC<Props> = (props) => {
       setOpen={setOpen}
       activeItem={activeItem}
       />
+
+      <Hero />
     </div>
   )
 };
